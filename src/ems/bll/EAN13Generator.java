4c7931b71310b04b1637b46f651c42268aa84563
RomanMeasv <90683512+RomanMeasv@users.noreply.github.com>
2022-03-14 18:20:51 +0100

    Code Generators
    Added EAN13 & QR Code Generators

---
+++ src/ems/bll/EAN13Generator.java
@@ -0,0 +1,26 @@
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.oned.EAN13Writer;
+
+import java.awt.image.BufferedImage;
+import java.nio.file.Paths;
+
+public class EAN13Generator {
+
+    //generateEAN13BarcodeImage("0123456789012", "ean13.png");
+    public static void generateEAN13BarcodeImage(String barcodeText, String path) throws Exception {
+        EAN13Writer barcodeWriter = new EAN13Writer();
+        BitMatrix bitMatrix =
+                barcodeWriter.encode(barcodeText, BarcodeFormat.EAN_13, 300, 150);
+
+        MatrixToImageWriter.writeToPath(bitMatrix, "png", Paths.get(path));
+    }
+
+    public static BufferedImage generateEAN13BarcodeImage(String barcodeText) throws Exception {
+        EAN13Writer barcodeWriter = new EAN13Writer();
+        BitMatrix bitMatrix = barcodeWriter.encode(barcodeText, BarcodeFormat.EAN_13, 300, 150);
+
+        return MatrixToImageWriter.toBufferedImage(bitMatrix);
+    }
+}

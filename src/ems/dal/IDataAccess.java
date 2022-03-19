79857fe57d75ef33c8b3e04ae270c1e02d679999
Ladam0203 <adam7605@easv365.dk>
2022-03-19 10:36:30 +0100

    Added createEventCoordinator on the facade

--- src/ems/dal/IDataAccess.java
+++ src/ems/dal/IDataAccess.java
@@ -1,8 +1,10 @@
 package ems.dal;
 
+import ems.be.EventCoordinator;
 import ems.be.User;
 
 
 public interface IDataAccess {
     User tryLogin(String username, String password) throws Exception;
+    EventCoordinator createEventCoordinator(EventCoordinator ec) throws Exception;
 }

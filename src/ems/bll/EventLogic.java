8cbc3f9735ff4c9e90a5ee472030d62e35803fc9
Miloš Jozek <90444097+BoldHeadBall@users.noreply.github.com>
2022-03-21 10:48:22 +0100

    Created event is added to list
    Also the is semi done delete event implementation, need to be done from my side

--- src/ems/bll/EventLogic.java
+++ src/ems/bll/EventLogic.java
@@ -4,6 +4,8 @@ import ems.be.Event;
 import ems.dal.DAFacade;
 import ems.dal.IDataAccess;
 
+import java.util.List;
+
 public class EventLogic {
     IDataAccess dataAccess;
 
@@ -11,5 +13,9 @@ public class EventLogic {
 
     public void createEvent(Event e) throws Exception {
         dataAccess.createEvent(e);
+    }
+
+    public List<Event> readAllEvents() throws Exception {
+        return dataAccess.readAllEvents();
     }
 }
--- src/ems/dal/IDataAccess.java
+++ src/ems/dal/IDataAccess.java
@@ -4,8 +4,10 @@ import ems.be.Event;
 import ems.be.EventCoordinator;
 import ems.be.User;
 import ems.gui.view.ECDialog;
+import jdk.jshell.spi.ExecutionControlProvider;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 
 public interface IDataAccess {
@@ -16,4 +18,6 @@ public interface IDataAccess {
     void deleteEventCoordinator(EventCoordinator ec) throws Exception;
     List<EventCoordinator> filterEventCoordinators(String query) throws Exception;
     Event createEvent (Event e) throws Exception;
+    void deleteEvent (Event e) throws Exception;
+    List<Event> readAllEvents() throws Exception;
 }

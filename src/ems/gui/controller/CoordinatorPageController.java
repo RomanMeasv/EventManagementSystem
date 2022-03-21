6aa4a86dfb24177ef637ff39376db0b8d25c729c
RomanMeasv <90683512+RomanMeasv@users.noreply.github.com>
2022-03-21 09:59:32 +0100

    Update CoordinatorPageController.java

--- src/ems/gui/controller/CoordinatorPageController.java
+++ src/ems/gui/controller/CoordinatorPageController.java
@@ -1,4 +1,29 @@
 package ems.gui.controller;
 
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+
 public class CoordinatorPageController {
+    public TextField txfFilter;
+    public TableView tbvEvents;
+    public TableColumn colEvents;
+
+    public void handleCreate(MouseEvent mouseEvent) {
+
+    }
+
+    public void handleDelete(MouseEvent mouseEvent) {
+
+    }
+
+    public void handleUpdate(MouseEvent mouseEvent) {
+
+    }
+
+    public void handleFilter(KeyEvent keyEvent) {
+        
+    }
 }

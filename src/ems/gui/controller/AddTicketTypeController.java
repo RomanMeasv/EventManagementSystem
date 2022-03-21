50bfb2c4394b1282b5f7f5af3f61a75c3d554b49
Miloš Jozek <90444097+BoldHeadBall@users.noreply.github.com>
2022-03-21 09:57:45 +0100

    Create event dialog
    gui and dialog class and controller

---
+++ src/ems/gui/controller/AddTicketTypeController.java
@@ -0,0 +1,34 @@
+package ems.gui.controller;
+
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.DialogPane;
+
+import java.io.IOException;
+
+public class AddTicketTypeController extends Dialog {
+
+    public AddTicketTypeController controller;
+    public AddTicketTypeController(){
+        super();
+        try{
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("addTicketTypeDialogView.fxml"));
+            DialogPane dp = loader.load();
+            controller = loader.getController();
+            this.setTitle("Add Ticket");
+            this.setDialogPane(dp);
+            this.setResultConverter(buttonType -> {
+                if(buttonType == ButtonType.APPLY){
+                    return null;
+                }
+                return null;
+            });
+
+        } catch (IOException ioex){
+            //System.out.println("Couldn't load view!");
+        }
+    }
+
+}

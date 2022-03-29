d5024ff3ee64f46f01352a623b2129bdc7191186
Miloš Jozek <90444097+BoldHeadBall@users.noreply.github.com>
2022-03-29 15:28:24 +0200

    event redid not tested

--- src/ems/gui/controller/EventCoordinatorPageControllerNew.java
+++ src/ems/gui/controller/EventCoordinatorPageControllerNew.java
@@ -1,21 +1,28 @@
 package ems.gui.controller;
 
-import javafx.event.Event;
+import ems.be.Event;
+import ems.gui.model.EventModel;
+import ems.gui.view.dialogs.EventDialog;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
+import java.util.concurrent.ExecutionException;
 
 public class EventCoordinatorPageControllerNew implements Initializable {
-    public TableView tbvEvents;
-    public TableColumn colEvents;
+    private EventModel eventModel;
+
+    public TableView<Event> tbvEvents;
+    public TableColumn<Event, String> colEvents;
     public TextField txfFilterEvents;
     public VBox boxEvents;
     public HBox boxEventsButtons;
@@ -34,26 +41,69 @@ public class EventCoordinatorPageControllerNew implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        try {
+            eventModel = new EventModel();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         boxEventsButtons.managedProperty().bind(boxEventsButtons.visibleProperty());
         boxCustomersButtons.managedProperty().bind(boxCustomersButtons.visibleProperty());
         boxTicketsButtons.managedProperty().bind(boxTicketsButtons.visibleProperty());
+
+        //Event set up
+        colEvents.setCellValueFactory(new PropertyValueFactory<>("name"));
+        tbvEvents.setItems(eventModel.getObservableEvents());
     }
 
     /* EVENTS */
     public void handleFilterEvents(KeyEvent keyEvent) {
-
+        try {
+            String query = txfFilterEvents.getText();
+            eventModel.filterEvents(query);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void handleCreateEvent(MouseEvent mouseEvent) {
-
+        EventDialog dialog = new EventDialog();
+        Optional<Event> result = dialog.showAndWait();
+        result.ifPresent(response -> {
+            try {
+                eventModel.createEvent(response);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
     }
 
     public void handleDeleteEvent(MouseEvent mouseEvent) {
-
+        try {
+            Event selected = tbvEvents.getSelectionModel().getSelectedItem();
+            if (selected != null) {
+                eventModel.deleteEvent(selected);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void handleUpdateEvent(MouseEvent mouseEvent) {
-
+        try {
+            Event oldEvent = tbvEvents.getSelectionModel().getSelectedItem();
+            if (oldEvent != null) {
+                EventDialog dialog = new EventDialog();
+                dialog.setFields(oldEvent);
+                Optional<Event> result = dialog.showAndWait();
+                if (result.isPresent()) {
+                    Event updatedEvent = result.get();
+                    updatedEvent.setId(oldEvent.getId());
+                    eventModel.updateEvent(oldEvent, updatedEvent);
+                }
+            }
+        } catch (Exception e) {
+            //don't do anything
+        }
     }
 
     /* CUSTOMERS */
@@ -96,7 +146,7 @@ public class EventCoordinatorPageControllerNew implements Initializable {
     }
 
     public void selectionChangedEventTab(Event event) {
-        
+
     }
 
     public void selectionChangedCustomerTab(Event event) {

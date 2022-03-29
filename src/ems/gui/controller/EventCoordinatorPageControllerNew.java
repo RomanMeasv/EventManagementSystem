43ee4cdea021d063d089a1170f16c1e3d5ae7337
Ladam0203 <adam7605@easv365.dk>
2022-03-29 15:39:43 +0200

    Parametirezed table views and cols in controller

--- src/ems/gui/controller/EventCoordinatorPageControllerNew.java
+++ src/ems/gui/controller/EventCoordinatorPageControllerNew.java
@@ -33,8 +33,8 @@ public class EventCoordinatorPageControllerNew implements Initializable {
     public VBox boxEvents;
     public HBox boxEventsButtons;
 
-    public TableView tbvCustomers;
-    public TableColumn colCustomers;
+    public TableView<Customer> tbvCustomers;
+    public TableColumn<Customer, String> colCustomers;
     public TextField txfFilterCustomers;
     public VBox boxCustomers;
     public HBox boxCustomersButtons;

932ba3e21d05cf5459b14cb760495a1ff64eeb14
RomanMeasv <90683512+RomanMeasv@users.noreply.github.com>
2022-04-05 19:10:58 +0200

    Update EventCoordinatorPageController.java

--- src/ems/gui/controller/EventCoordinatorPageController.java
+++ src/ems/gui/controller/EventCoordinatorPageController.java
@@ -8,30 +8,6 @@ import java.util.ResourceBundle;
 public class EventCoordinatorPageController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-                /* Disable cancel/apply buttons */
-        /*btnApplyEvent.setDisable(true);
-        btnCancelEvent.setDisable(true);
-        btnApplyCustomer.setDisable(true);
-        btnCancelCustomer.setDisable(true);
-        btnApplyTicket.setDisable(true);
-        btnCancelTicket.setDisable(true);*/
 
-        //hide ticket creation/edit specific fields
-        /*lblTicketUUID.setVisible(false);
-        txfNoTickets.setVisible(false);
-        chbTicketValidation.setVisible(false);*/
     }
-
-    /*private void refreshTickets()
-    {
-        try{
-            ticketModel.clearFilter();
-            tbvOverviewTickets.getItems().clear();
-            tbvOverviewTickets.getItems().addAll(ticketModel.getObservableTickets());
-            tbvTicketTabTickets.getItems().clear();
-            tbvTicketTabTickets.getItems().addAll(ticketModel.getObservableTickets());
-        } catch (Exception ex){
-            PopUp.showError(ex.getMessage());
-        }
-    }*/
 }

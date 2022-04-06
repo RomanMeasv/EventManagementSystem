4ec3e10a69be35b992abbc6d440b5f3d5d3c80a7
Miloš Jozek <90444097+BoldHeadBall@users.noreply.github.com>
2022-04-06 20:04:10 +0200

    admin coordinators tapppppppp work in progress

--- src/ems/gui/controller/adminPage/tabs/CoordinatorTabController.java
+++ src/ems/gui/controller/adminPage/tabs/CoordinatorTabController.java
@@ -1,8 +1,14 @@
 package ems.gui.controller.adminPage.tabs;
 
+import com.google.zxing.qrcode.decoder.Mode;
+import ems.be.Event;
+import ems.be.EventCoordinator;
+import ems.gui.model.EventCoordinatorModel;
+import ems.gui.model.ModelFacade;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
@@ -11,43 +17,55 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class CoordinatorTabController implements Initializable {
-    public ListView ltvTickets;
-    public TextField txfFilterTickets;
+
+
+    public ListView <EventCoordinator> ltvCoordinators;
+    public TextField txfFilterCoordinators;
     public TextField txfFilterEvents;
-    public ListView ltvEventTicketTypes;
-    public TextField txfEventTicketType;
-    public Button btnCancelTicket;
-    public Button btnApplyTicket;
+    public ComboBox cmbEvents;
+    public TextField txfEventCoordinator;
+    public Button btnCancelCoordinator;
+    public Button btnApplyCoordianator;
+    public ListView <Event>ltvCoordinatorsEvents;
+
+    private ModelFacade facade;
+    private EventCoordinatorModel eventCoordinatorModel;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        try {
+            facade = ModelFacade.getInstance();
+            eventCoordinatorModel = new EventCoordinatorModel();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        ltvCoordinators.setItems(eventCoordinatorModel.getAllEventCoordinators());
 
+    }
+
+
+    public void handleNewCoordianator(ActionEvent event) {
+    }
+
+    public void handleRemoveCoordianator(ActionEvent event) {
     }
 
     public void handleFilterEvents(KeyEvent keyEvent) {
     }
 
-    public void handleFilterTickets(KeyEvent keyEvent) {
+
+    public void handleAddEvent(ActionEvent event) {
     }
 
-    public void handleNewTicket(ActionEvent event) {
+    public void handleRemoveEvent(ActionEvent event) {
     }
 
-    public void handleRemoveTicket(ActionEvent event) {
+    public void handleFilterCoordinators(KeyEvent keyEvent) {
     }
 
-    public void handleFilterTicketTypes(KeyEvent keyEvent) {
+    public void handleCancelCoordianator(ActionEvent event) {
     }
 
-    public void handleAddTicketType(ActionEvent event) {
-    }
-
-    public void handleRemoveTicketType(ActionEvent event) {
-    }
-
-    public void handleCancelTicket(ActionEvent event) {
-    }
-
-    public void handleApplyTicket(ActionEvent event) {
+    public void handleApplyCoordianator(ActionEvent event) {
     }
 }

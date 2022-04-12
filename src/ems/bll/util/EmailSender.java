742beefcde241550e3664200196837f5c89710cc
Ladam0203 <adam7605@easv365.dk>
2022-04-12 13:05:02 +0200

    Outlook path is dynamically found

--- src/ems/bll/util/EmailSender.java
+++ src/ems/bll/util/EmailSender.java
@@ -2,12 +2,43 @@ package ems.bll.util;
 
 import ems.be.Ticket;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class EmailSender {
-    public static void sendTicket(String recipient, Ticket ticket, File ticketFile) throws IOException {
-        String outlookPath = "C:\\Program Files\\Microsoft Office\\root\\Office16\\OUTLOOK.EXE";
+    private static String getOutlookPath() throws Exception{
+        Process p = Runtime.getRuntime()
+                .exec(new String[]{"cmd.exe", "/c", "assoc", ".pst"});
+        BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
+        String extensionType = input.readLine();
+        input.close();
+        // extract type
+        if (extensionType == null) {
+            throw new IOException("Could not find extension type!");
+        } else {
+            String[] fileType = extensionType.split("=");
+
+            p = Runtime.getRuntime().exec(
+                    new String[]{"cmd.exe", "/c", "ftype", fileType[1]});
+            input = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String fileAssociation = input.readLine();
+            // extract path
+            Pattern pattern = Pattern.compile("\".*?\"");
+            Matcher m = pattern.matcher(fileAssociation);
+            if (m.find()) {
+                return m.group(0);
+            } else {
+                throw new IOException("Could not find outlook path!");
+            }
+        }
+    }
+
+    public static void sendTicket(String recipient, Ticket ticket, File ticketFile) throws Exception {
+        String outlookPath = getOutlookPath();
 
         String subject = "Your ticket for event " + ticket.getEvent().getName();
         String body = "Dear " + ticket.getCustomer().getName() + ",\n" +
